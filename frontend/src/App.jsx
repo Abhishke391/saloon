fd15562d8a74069501062901103cb2ccbe98784a
Pulasthi Abishake <pulasthiabishake06@gmail.com>
2024-11-04 09:00:43 +0530

    adding header and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,12 @@ import Login from "./pages/Login";
 import MyAppointment from "./pages/MyAppointment";
 import MyProfile from "./pages/MyProfile";
 import Appointments from "./pages/Appointments";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors/:speciality" element={<Doctors />} />

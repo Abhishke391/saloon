96d6527be74b7b6a1f269f98fd8a6d6e6b12121b
Abhishke391 <pulasthiabishake06@gmail.com>
2024-11-24 20:12:01 +0530

    finishing ui parts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Doctors from "./pages/Doctors";
 import About from "./pages/About";
-import Content from "./pages/Content";
 import Login from "./pages/Login";
 import MyAppointment from "./pages/MyAppointment";
 import MyProfile from "./pages/MyProfile";
 import Appointments from "./pages/Appointments";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -19,10 +19,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/doctors/:speciality?" element={<Doctors />} />
         <Route path="/About" element={<About />} />
-        <Route path="/content" element={<Content />} />
         <Route path="/login" element={<Login />} />
         <Route path="/myappointment" element={<MyAppointment />} />
-        <Route path="/content" element={<Content />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/myprofile" element={<MyProfile />} />
         <Route path="/appointment/:docId?" element={<Appointments />} />
       </Routes>
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets/assets_frontend/assets";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <div className="text-center text-2xl pt-10 text-gray-500">
+        <p>
+          ABOUT <span className="text-gray-700 font-medium">US</span>
+        </p>
+      </div>
 
-export default About
+      <div className="my-10 flex flex-col md:flex-row gap-12">
+        <img
+          className="w-full md:max-w-[360px]"
+          src={assets.about_image}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente,
+            dolor ex adipisci magni numquam aliquam eius magnam, ducimus libero
+            tempore iste suscipit omnis laboriosam cum harum sint? Nihil, libero
+            necessitatibus corrupti placeat quae similique reiciendis porro
+            harum magni expedita tenetur, dolores deserunt ad maxime, nam minima
+            sunt iste quidem dolorum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            dolor, nam velit laudantium qui officia beatae, omnis dolores
+            eveniet repellendus itaque recusandae eaque nostrum mollitia
+            assumenda adipisci exercitationem illo ullam? Nam voluptates, sunt
+            placeat magnam explicabo vero quo quam soluta veritatis, iure
+            obcaecati unde aliquid suscipit tenetur dolores commodi id.
+          </p>
+          <b>Our Vision</b>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
+            doloribus tempora aperiam unde nobis ipsa dicta impedit molestias.
+            Quaerat officia repudiandae explicabo ex, saepe exercitationem
+            possimus et maxime aut neque!
+          </p>
+        </div>
+      </div>
+
+      <div className="text-xl my-4">
+        <p>
+          WHY <span className="text-gray-700 font-semibold">CHOOSE US</span>
+        </p>
+      </div>
+
+      <div className="flex flex-col md:flex-row mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Efficiency:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, soluta!</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer">
+          <b>Convenience:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, quo!</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer" >
+          <b>Personalization:</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, id.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;

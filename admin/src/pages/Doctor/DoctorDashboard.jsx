8e6e53277b50413e7c8a25f1ec7a27486fe70f60
Abhishke391 <pulasthiabishake06@gmail.com>
2025-01-04 23:02:46 +0530

    finishing all the parts

--- admin/src/pages/Doctor/DoctorDashboard.jsx
+++ admin/src/pages/Doctor/DoctorDashboard.jsx
@@ -1,33 +1,34 @@
 import React from "react";
 import { useContext } from "react";
-import { AdminContext } from "../../context/AdminContext";
+import { DoctorContext } from "../../context/DoctorContext";
 import { useEffect } from "react";
 import { assets } from "../../assets/assets";
 import { AppContext } from "../../context/AppContext";
 
-const Dashboard = () => {
-  const { aToken, dashData, getDashData, cancelAppointment } =
-    useContext(AdminContext);
+const DoctorDashboard = () => {
+  const { dToken, dashData, setDashData, getDashData, cancelAppointment, completeAppointment } =
+    useContext(DoctorContext);
 
-  const { slotDateFormat } = useContext(AppContext);
+  const { currency, slotDateFormat } = useContext(AppContext);
 
   useEffect(() => {
-    if (aToken) {
+    if (dToken) {
       getDashData();
     }
-  }, [aToken]);
+  }, [dToken]);
 
   return (
     dashData && (
       <div className="m-5">
         <div className="flex flex-wrap gap-3">
           <div className="flex items-center gap-2 bg-white p-4 min-w-52 border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
-            <img className="w-14" src={assets.doctor_icon} alt="" />
+            <img className="w-14" src={assets.earning_icon} alt="" />
             <div>
               <p className="text-xl font-semibold text-gray-600">
-                {dashData.doctors}
+                {currency}
+                {dashData.earnings}
               </p>
-              <p className="text-gray-400">Doctors</p>
+              <p className="text-gray-400">Earnings</p>
             </div>
           </div>
 
@@ -53,24 +54,24 @@ const Dashboard = () => {
         </div>
 
         <div className="bg-white">
-          <div className="flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border ">
+          <div className="flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border">
             <img src={assets.list_icon} alt="" />
             <p className="font-semibold">Latest Booking</p>
           </div>
 
           <div className="pt-4 border border-t-0">
-            {dashData.latestAppointment.map((item, index) => (
+            {dashData.latestAppointments.map((item, index) => (
               <div
                 className="flex items-center px-6 py-3 gap-3 hover:bg-gray-100"
                 key={index}
               >
                 <img
                   className="rounded-full w-10"
-                  src={item.docData.image}
+                  src={item.userData.image}
                   alt=""
                 />
                 <div className="flex-1 text-sm">
-                  <p className="text-gray-800">{item.docData.name}</p>
+                  <p className="text-gray-800">{item.userData.name}</p>
                   <p className="text-gray-600">
                     {slotDateFormat(item.slotDate)}
                   </p>
@@ -82,12 +83,20 @@ const Dashboard = () => {
                     Completed
                   </p>
                 ) : (
-                  <img
-                    onClick={() => cancelAppointment(item._id)}
-                    className="w-10 cursor-pointer"
-                    src={assets.cancel_icon}
-                    alt=""
-                  />
+                  <div className="flex">
+                    <img
+                      onClick={() => cancelAppointment(item._id)}
+                      className="w-10 cursor-pointer"
+                      src={assets.cancel_icon}
+                      alt=""
+                    />
+                    <img
+                      onClick={() => completeAppointment(item._id)}
+                      className="w-10 cursor-pointer"
+                      src={assets.tick_icon}
+                      alt=""
+                    />
+                  </div>
                 )}
               </div>
             ))}
@@ -98,4 +107,4 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+export default DoctorDashboard;
